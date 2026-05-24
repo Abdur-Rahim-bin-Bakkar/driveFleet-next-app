0290ef3e149e49c6244eee09cc1b9f983b777c21
webdesignrahim4061 <webdesignrahim4061@gmail.com>
2026-05-24 16:13:33 +0600

    change in my added page

--- src/app/my-added-cars/page.jsx
+++ src/app/my-added-cars/page.jsx
@@ -19,7 +19,7 @@ const MyAddedPage = async () => {
     console.log(myAddCars)
     return (
         <div className='max-w-11/12 mx-auto mt-15 bg-[#36ADA330] p-5'>
-            <h1 className='text-3xl md:text-4xl font-bold text-center my-4'>My Added Cars</h1>
+            <h1 className='text-3xl md:text-4xl font-bold text-center my-6'>My Added Cars</h1>
             <div className='mt-10 space-y-4'>
                 {
                     myAddCars.map(car => <AddCarCard key={car?._id} car={car} />)
